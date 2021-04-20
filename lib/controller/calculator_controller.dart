253c642d2b31cf63682e04181e6a9467fc5f5fb4
Beacko <beacko28@gmail.com>
2021-04-20 18:59:52 -0300

    Calculadora_Piso
    Calculadora de pisos

---
+++ lib/controller/calculator_controller.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_application_1/helpers/parse_helper.dart';
+import 'package:flutter_application_1/models/floor_model.dart';
+import 'package:flutter_application_1/models/result_model.dart';
+import 'package:flutter_application_1/models/room_model.dart';
+
+class CalculatorController {
+  final RoomModel _room = RoomModel();
+  final FloorModel _floor = FloorModel();
+
+  void setRoomWidth(String value) {
+    _room.width = ParseHelper.toDouble(value);
+  }
+
+  void setRoomLength(String value) {
+    _room.width = ParseHelper.toDouble(value);
+  }
+
+  void setFloorWidth(String value) {
+    _room.width = ParseHelper.toDouble(value);
+  }
+
+  void setFloorLength(String value) {
+    _room.width = ParseHelper.toDouble(value);
+  }
+
+  void setFloorPrice(String value) {
+    _floor.price = ParseHelper.toDouble(value);
+  }
+
+  ResultModel calculate() {
+    return ResultModel(
+      piecesByWidth: (_room.width / _floor.width).ceil(),
+      piecesByLength: (_room.length / _floor.length).ceil(),
+      areaFloor: _floor.width * _floor.width,
+      price: _floor.price * (_floor.width * _floor.width),
+    );
+  }
+}
